eec58e30665a625cca7617dbb32787e9af9affdb
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-04 12:16:34 -0800

    Sony_DriversV1.cpp
    An archive of the first version of the Sony Drivers, written in C++

---
+++ CamCam+/Drivers/OldVersions/Sony_DriversV1.cpp
@@ -0,0 +1,346 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+// Start of script
+
+/* Special characters used
+* α - Alpha (Greek) U+03B1
+*/
+
+int main() {
+  // Drivers for the Sony α series of camera
+}
+int Mode() {
+  bool laptopMode = false;
+  bool desktopMode = false;
+  bool tabletMode = false;
+  bool phoneMode = false;
+  bool cameraMode = true;
+}
+int microphone() {
+  bool microphoneSupport = true;
+  bool microphoneExternal = false;
+  bool microphoneInternal = true;
+}
+int models() {
+  // Sony α model support
+  bool Sony_α33 = true;
+  bool Sony_α35 = true;
+  bool Sony_α37 = true;
+  bool Sony_α55 = true;
+  bool Sony_α57 = true;
+  bool Sony_α58 = true;
+  bool Sony_α65 = true;
+  bool Sony_α68 = true;
+  bool Sony_α77 = true;
+  bool Sony_α77ii = true;
+  bool Sony_α99 = true;
+  bool Sony_α99ii = true;
+  bool Sony_α100 = true;
+  bool Sony_α200 = true;
+  bool Sony_α230 = true;
+  bool Sony_α290 = true;
+  bool Sony_α300 = true;
+  bool Sony_α330 = true;
+  bool Sony_α350 = true;
+  bool Sony_α380 = true;
+  bool Sony_α390 = true;
+  bool Sony_α450 = true;
+  bool Sony_α500 = true;
+  bool Sony_α550 = true;
+  bool Sony_α560 = true;
+  bool Sony_α580 = true;
+  bool Sony_α700 = true;
+  bool Sony_α850 = true;
+  bool Sony_α900 = true;
+}
+int resolution() {
+  // Sony α resolution
+  // α33
+  int Sonyα33_x = 1280; // X axis of Sony α33
+  int Sonyα33_y = 720; // Y axis of Sony α33
+  // α35
+  int Sonyα35_x = 1920;  // X axis of Sony α35
+  int Sonyα35_y = 1080;  // Y axis of Sony α35
+  // α37
+  int Sonyα37_x = 1920;  // X axis of Sony α37
+  int Sonyα37_y = 1080;  // Y axis of Sony α37
+  // α55
+  int Sonyα55_x = 1920;  // X axis of Sony α55
+  int Sonyα55_y = 1080;  // Y axis of Sony α55
+  // α57
+  int Sonyα57_x = 1920;  // X axis of Sony α57
+  int Sonyα57_y = 1080;  // Y axis of Sony α57
+  // α58
+  int Sonyα58_x = 1920;  // X axis of Sony α58
+  int Sonyα58_y = 1080;  // Y axis of Sony α58
+  // α65
+  int Sonyα65_x = 1920;  // X axis of Sony α65
+  int Sonyα65_y = 1080;  // Y axis of Sony α65
+  // α68
+  int Sonyα68_x = 1920;  // X axis of Sony α68
+  int Sonyα68_y = 1080;  // Y axis of Sony α68
+  // α77
+  int Sonyα77_x = 1920;  // X axis of Sony α77
+  int Sonyα77_y = 1080;  // Y axis of Sony α77
+  // α77ii
+  int Sonyα77ii_x = 1920;  // X axis of Sony α77 ii
+  int Sonyα77ii_y = 1080;  // Y axis of Sony α77 ii
+  // α99
+  int Sonyα99_x = 1920;  // X axis of Sony α99
+  int Sonyα99_y = 1080;  // Y axis of Sony α99
+  // α99ii
+  int Sonyα99ii_x = 1920;  // X axis of Sony α99 ii
+  int Sonyα99ii_y = 1080;  // Y axis of Sony α99 ii
+  // α100
+  int Sonyα100_x = 1920;  // X axis of Sony α100
+  int Sonyα100_y = 1080;  // Y axis of Sony α100
+  // α200
+  int Sonyα200_x = 1920;  // X axis of Sony α200
+  int Sonyα200_y = 1080;  // Y axis of Sony α200
+  // α230
+  int Sonyα230_x = 1920;  // X axis of Sony α230
+  int Sonyα230_y = 1080;  // Y axis of Sony α230
+  // α290
+  int Sonyα290_x = 1920;  // X axis of Sony α290
+  int Sonyα290_y = 1080;  // Y axis of Sony α290
+  // α300
+  int Sonyα300_x = 1920;  // X axis of Sony α300
+  int Sonyα300_y = 1080;  // Y axis of Sony α300
+  // α330
+  int Sonyα330_x = 1920;  // X axis of Sony α330
+  int Sonyα330_y = 1080;  // Y axis of Sony α330
+  // α350
+  int Sonyα350_x = 1920;  // X axis of Sony α350
+  int Sonyα350_y = 1080;  // Y axis of Sony α350
+  // α380
+  int Sonyα380_x = 1920;  // X axis of Sony α380
+  int Sonyα380_y = 1080;  // Y axis of Sony α380
+  // α390
+  int Sonyα390_x = 1920;  // X axis of Sony α390
+  int Sonyα390_y = 1080;  // Y axis of Sony α390
+  // α450
+  int Sonyα450_x = 1920;  // X axis of Sony α450
+  int Sonyα450_y = 1080;  // Y axis of Sony α450
+  // α500
+  int Sonyα500_x = 1920;  // X axis of Sony α500
+  int Sonyα500_y = 1080;  // Y axis of Sony α500
+  // α550
+  int Sonyα550_x = 1920;  // X axis of Sony α550
+  int Sonyα550_y = 1080;  // Y axis of Sony α550
+  // α560
+  int Sonyα560_x = 1920;  // X axis of Sony α560
+  int Sonyα560_y = 1080;  // Y axis of Sony α560
+  // α580
+  int Sonyα580_x = 1920;  // X axis of Sony α580
+  int Sonyα580_y = 1080;  // Y axis of Sony α580
+  // α700
+  int Sonyα700_x = 1920;  // X axis of Sony α700
+  int Sonyα700_y = 1080;  // Y axis of Sony α700
+  // α850
+  int Sonyα850_x = 1920;  // X axis of Sony α850
+  int Sonyα850_y = 1080;  // Y axis of Sony α850
+  // α900
+  int Sonyα900_x = 1920;  // X axis of Sony α900
+  int Sonyα900_y = 1080;  // Y axis of Sony α900
+}
+int framerate() {
+  // Sony camera frames per second
+  // α33
+  int Sonyα33_fps = 30; // Framerate (FPS) of Sony α α33
+  // α35
+  int Sonyα35_fps = 30; // Framerate (FPS) of Sony α α35
+  // α37
+  int Sonyα37_fps = 30; // Framerate (FPS) of Sony α α37
+  // α55
+  int Sonyα55_fps = 30; // Framerate (FPS) of Sony α α55
+  // α57
+  int Sonyα57_fps = 30; // Framerate (FPS) of Sony α α57
+  // α58
+  int Sonyα58_fps = 30; // Framerate (FPS) of Sony α α58
+  // α65
+  int Sonyα65_fps = 30; // Framerate (FPS) of Sony α α65
+  // α68
+  int Sonyα68_fps = 30; // Framerate (FPS) of Sony α α68
+  // α77
+  int Sonyα77_fps = 30; // Framerate (FPS) of Sony α α77
+  // α77ii
+  int Sonyα77ii_fps = 30; // Framerate (FPS) of Sony α α77ii
+  // α99
+  int Sonyα99_fps = 30; // Framerate (FPS) of Sony α α99
+  // α99ii
+  int Sonyα99ii_fps = 30; // Framerate (FPS) of Sony α α99ii
+  // α100
+  int Sonyα100_fps = 60; // Framerate (FPS) of Sony α α100
+  // α200
+  int Sonyα200_fps = 60; // Framerate (FPS) of Sony α α200
+  // α230
+  int Sonyα230_fps = 60; // Framerate (FPS) of Sony α α230
+  // α290
+  int Sonyα290_fps = 60; // Framerate (FPS) of Sony α α290
+  // α300
+  int Sonyα300_fps = 60; // Framerate (FPS) of Sony α α300
+  // α330
+  int Sonyα330_fps = 60; // Framerate (FPS) of Sony α α330
+  // α350
+  int Sonyα350_fps = 60; // Framerate (FPS) of Sony α α350
+  // α380
+  int Sonyα380_fps = 60; // Framerate (FPS) of Sony α α380
+  // α390
+  int Sonyα390_fps = 60; // Framerate (FPS) of Sony α α390
+  // α450
+  int Sonyα450_fps = 60; // Framerate (FPS) of Sony α α450
+  // α500
+  int Sonyα500_fps = 60; // Framerate (FPS) of Sony α α500
+  // α560
+  int Sonyα560_fps = 60; // Framerate (FPS) of Sony α α560
+  // α580
+  int Sonyα580_fps = 60; // Framerate (FPS) of Sony α α580
+  // α700
+  int Sonyα700_fps = 60; // Framerate (FPS) of Sony α α700
+  // α850
+  int Sonyα850_fps = 60; // Framerate (FPS) of Sony α α850
+  // α900
+  int Sonyα900_fps = 60; // Framerate (FPS) of Sony α α900
+}
+int bitDepth() {
+  // Sony camera bit depth
+  // α33
+  int Sonyα33_bitd = 24; // Bit depth of Sony α33
+  // α35
+  int Sonyα35_bitd = 24; // Bit depth of Sony α35
+  // α37
+  int Sonyα37_bitd = 24; // Bit depth of Sony α37
+  // α55
+  int Sonyα55_bitd = 24; // Bit depth of Sony α55
+  // α57
+  int Sonyα57_bitd = 24; // Bit depth of Sony 57
+  // α58
+  int Sonyα58_bitd = 24; // Bit depth of Sony α58
+  // α65
+  int Sonyα65_bitd = 24; // Bit depth of Sony α65
+  // α68
+  int Sonyα68_bitd = 24; // Bit depth of Sony α68
+  // α77
+  int Sonyα77_bitd = 24; // Bit depth of Sony α77
+  // α77ii
+  int Sonyα77ii_bitd = 24; // Bit depth of Sony α77ii
+  // α99
+  int Sonyα99_bitd = 24; // Bit depth of Sony α99
+  // α99ii
+  int Sonyα99ii_bitd = 24; // Bit depth of Sony α99ii
+  // α100
+  int Sonyα100_bitd = 24; // Bit depth of Sony α100
+  // α200
+  int Sonyα200_bitd = 24; // Bit depth of Sony α200
+  // α230
+  int Sonyα230_bitd = 24; // Bit depth of Sony α230
+  // α290
+  int Sonyα290_bitd = 24; // Bit depth of Sony α290
+  // α300
+  int Sonyα300_bitd = 24; // Bit depth of Sony α300
+  // α330
+  int Sonyα330_bitd = 24; // Bit depth of Sony α330
+  // α350
+  int Sonyα350_bitd = 24; // Bit depth of Sony α350
+  // α380
+  int Sonyα380_bitd = 24; // Bit depth of Sony α380
+  // α390
+  int Sonyα390_bitd = 24; // Bit depth of Sony α390
+  // α450
+  int Sonyα450_bitd = 24; // Bit depth of Sony α450
+  // α500
+  int Sonyα500_bitd = 24; // Bit depth of Sony α500
+  // α560
+  int Sonyα560_bitd = 24; // Bit depth of Sony α560
+  // α580
+  int Sonyα580_bitd = 24; // Bit depth of Sony α580
+  // α700
+  int Sonyα700_bitd = 24; // Bit depth of Sony α700
+  // α850
+  int Sonyα850_bitd = 24; // Bit depth of Sony α850
+  // α900
+  int Sonyα900_bitd = 24; // Bit depth of Sony α900
+}
+int fileFormat() {
+  // File format compatibility for Sony α33
+  int Sonyα33_fformat = "*.mp4"; // Default file format for Sony α33
+  // File format compatibility for Sony α35
+  int Sonyα35_fformat = "*.mp4"; // Default file format for Sony α35
+  // File format compatibility for Sony α37
+  int Sonyα37_fformat = "*.mp4"; // Default file format for Sony α37
+  // File format compatibility for Sony α55
+  int Sonyα55_fformat = "*.mp4"; // Default file format for Sony α55
+  // File format compatibility for Sony α57
+  int Sonyα57_fformat = "*.mp4"; // Default file format for Sony α57
+  // File format compatibility for Sony α58
+  int Sonyα58_fformat = "*.mp4"; // Default file format for Sony α58
+  // File format compatibility for Sony α65
+  int Sonyα65_fformat = "*.mp4"; // Default file format for Sony α65
+  // File format compatibility for Sony α68
+  int Sonyα68_fformat = "*.mp4"; // Default file format for Sony α68
+  // File format compatibility for Sony α77
+  int Sonyα77_fformat = "*.mp4"; // Default file format for Sony α77
+  // File format compatibility for Sony α77ii
+  int Sonyα77ii_fformat = "*.mp4"; // Default file format for Sony α77ii
+  // File format compatibility for Sony α99
+  int Sonyα99_fformat = "*.mp4"; // Default file format for Sony α99
+  // File format compatibility for Sony α99ii
+  int Sonyα99ii_fformat = "*.mp4"; // Default file format for Sony α99ii
+  // File format compatibility for Sony α100
+  int Sonyα100_fformat = "*.mp4"; // Default file format for Sony α100
+  // File format compatibility for Sony α200
+  int Sonyα200_fformat = "*.mp4"; // Default file format for Sony α200
+  // File format compatibility for Sony α230
+  int Sonyα230_fformat = "*.mp4"; // Default file format for Sony α230
+  // File format compatibility for Sony α290
+  int Sonyα290_fformat = "*.mp4"; // Default file format for Sony α290
+  // File format compatibility for Sony α300
+  int Sonyα300_fformat = "*.mp4"; // Default file format for Sony α300
+  // File format compatibility for Sony α330
+  int Sonyα330_fformat = "*.mp4"; // Default file format for Sony α330
+  // File format compatibility for Sony α350
+  int Sonyα350_fformat = "*.mp4"; // Default file format for Sony α350
+  // File format compatibility for Sony α380
+  int Sonyα380_fformat = "*.mp4"; // Default file format for Sony α380
+  // File format compatibility for Sony α390
+  int Sonyα390_fformat = "*.mp4"; // Default file format for Sony α390
+  // File format compatibility for Sony α450
+  int Sonyα450_fformat = "*.mp4"; // Default file format for Sony α450
+  // File format compatibility for Sony α500
+  int Sonyα500_fformat = "*.mp4"; // Default file format for Sony α500
+  // File format compatibility for Sony α560
+  int Sonyα560_fformat = "*.mp4"; // Default file format for Sony α560
+  // File format compatibility for Sony α580
+  int Sonyα580_fformat = "*.mp4"; // Default file format for Sony α580
+  // File format compatibility for Sony α700
+  int Sonyα700_fformat = "*.mp4"; // Default file format for Sony α700
+  // File format compatibility for Sony α850
+  int Sonyα850_fformat = "*.mp4"; // Default file format for Sony α850
+  // File format compatibility for Sony α900
+  int Sonyα900_fformat = "*.mp4"; // Default file format for Sony α900
+}
+int exceptionHandlingTest() { // Exception handling test, from https://en.wikipedia.org/wiki/C%2B%2B#Exception_handling
+    try {
+        std::vector<int> vec{3, 4, 3, 1};
+        int i{vec.at(4)}; // Throws an exception, std::out_of_range (indexing for vec is from 0-3 not 1-4)
+    }
+    // An exception handler, catches std::out_of_range, which is thrown by vec.at(4)
+    catch (std::out_of_range &e) {
+        std::cerr << "Accessing a non-existent element: " << e.what() << '\n';
+    }
+    // To catch any other standard library exceptions (they derive from std::exception)
+    catch (std::exception &e) {
+        std::cerr << "Exception thrown: " << e.what() << '\n';
+    }
+    // Catch any unrecognised exceptions (i.e. those which don't derive from std::exception)
+    catch (...) {
+        std::cerr << "Some fatal error\n";
+    }
+}
+// End of script
+/* File info
+* File version: 1 (Tuesday, November 4th 2020 at 12:16 pm)
+* File type: C++ Script source file (*.cpp)
+* Line count (including blank lines and compiler line): 347
+*/
