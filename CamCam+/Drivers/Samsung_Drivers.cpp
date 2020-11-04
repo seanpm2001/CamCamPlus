70bc31e11d89a22de56466a748528c0c7ed29e0f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-04 12:02:01 -0800

    Samsung_Drivers.cpp
    The drivers for Samsung devices.

---
+++ CamCam+/Drivers/Samsung_Drivers.cpp
@@ -0,0 +1,244 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+// Start of script
+
+int main() {
+  // Drivers for the Samsung series of camera
+}
+int Mode() {
+  bool laptopMode = false;
+  bool desktopMode = false;
+  bool tabletMode = false;
+  bool phoneMode = true;
+  bool cameraMode = false;
+}
+int microphone() {
+  bool microphoneSupport = true;
+  bool microphoneExternal = false;
+  bool microphoneInternal = true;
+}
+int models() {
+  // Samsung Galaxy model support
+  bool Samsung_GalaxyS1 = true;
+  bool Samsung_GalaxyS2 = true;
+  bool Samsung_GalaxyS3 = true;
+  bool Samsung_GalaxyS4 = true;
+  bool Samsung_GalaxyS5 = true;
+  bool Samsung_GalaxyS6 = true;
+  bool Samsung_GalaxyS7 = true;
+  bool Samsung_GalaxyS7Edge = true;
+  bool Samsung_GalaxyS8 = true;
+  bool Samsung_GalaxyS8Plus = true;
+  bool Samsung_GalaxyS9 = true;
+  bool Samsung_GalaxyS9Plus = true;
+  bool Samsung_GalaxyS10 = true;
+  bool Samsung_GalaxyS10Plus = true;
+  bool Samsung_GalaxyS20 = true;
+  bool Samsung_GalaxyS20Plus = true;
+  bool Samsung_GalaxyS20Ultra = true;
+  bool Samsung_GalaxyS20FE = true;
+  // Samsung Note model support
+  bool Samsung_Note7 = true;
+  bool Samsung_Note8 = true;
+  bool Samsung_Note9 = true;
+  bool Samsung_Note10 = true;
+  bool Samsung_Note20 = true;
+}
+int resolution() {
+  // Samsung Galaxy resolution
+  // Galaxy S1
+  int SamsungS1_x = 480; // X axis of Samsung Galaxy S1
+  int SamsungS1_y = 360; // Y axis of Samsung Galaxy S1
+  // Galaxy S2
+  int SamsungS2_x = 600;  // X axis of Samsung Galaxy S2
+  int SamsungS2_y = 480;  // Y axis of Samsung Galaxy S2
+  // Galaxy S3
+  int SamsungS3_x = 1280;  // X axis of Samsung Galaxy S3
+  int SamsungS3_y = 720;  // Y axis of Samsung Galaxy S3
+  // Galaxy S4
+  int SamsungS4_x = 1920;  // X axis of Samsung Galaxy S4
+  int SamsungS4_y = 1080;  // Y axis of Samsung Galaxy S4
+  // Galaxy S5
+  int SamsungS5_x = 1020;  // X axis of Samsung Galaxy S5
+  int SamsungS5_y = 1080;  // Y axis of Samsung Galaxy S5
+  // Galaxy S6
+  int SamsungS6_x = 2560;  // X axis of Samsung Galaxy S6
+  int SamsungS6_y = 1440;  // Y axis of Samsung Galaxy S6
+  // Galaxy S7
+  int SamsungS7_x = 3840;  // X axis of Samsung Galaxy S7
+  int SamsungS7_y = 2160;  // Y axis of Samsung Galaxy S7
+  // Galaxy S7 Edge
+  int SamsungS7ed_x = 3840;  // X axis of Samsung Galaxy S7 Edge
+  int SamsungS7ed_y = 2160;  // Y axis of Samsung Galaxy S7 Edge
+  // Galaxy S8
+  int SamsungS8_x = 3840;  // X axis of Samsung Galaxy S8
+  int SamsungS8_y = 2160;  // Y axis of Samsung Galaxy S8
+  // Galaxy S8 Plus
+  int SamsungS8p_x = 3840;  // X axis of Samsung Galaxy S8+
+  int SamsungS8p_y = 2160;  // Y axis of Samsung Galaxy S8+
+  // Galaxy S9
+  int SamsungS9_x = 3840;  // X axis of Samsung Galaxy S9
+  int SamsungS9_y = 2160;  // Y axis of Samsung Galaxy S9
+  // Galaxy S9 Plus
+  int SamsungS9p_x = 3840;  // X axis of Samsung Galaxy S9+
+  int SamsungS9p_y = 2160;  // Y axis of Samsung Galaxy S9+
+  // Galaxy S10
+  int SamsungS10_x = 3840;  // X axis of Samsung Galaxy S10
+  int SamsungS10_y = 2160;  // Y axis of Samsung Galaxy S10
+  // Galaxy S10 Plus
+  int SamsungS10p_x = 3840;  // X axis of Samsung Galaxy S10+
+  int SamsungS10p_y = 2160;  // Y axis of Samsung Galaxy S10+
+  // Galaxy S20
+  int SamsungS20_x = 7680;  // X axis of Samsung Galaxy S20
+  int SamsungS20_y = 4320;  // Y axis of Samsung Galaxy S20
+  // Galaxy S20 Plus
+  int SamsungS20pl_x = 7680;  // X axis of Samsung Galaxy S20+
+  int SamsungS20pl_y = 4320;  // Y axis of Samsung Galaxy S20+
+  // Galaxy S20 Ultra
+  int SamsungS20ultra_x = 7680;  // X axis of Samsung Galaxy S20 Ultra
+  int SamsungS20ultra_y = 4320;  // Y axis of Samsung Galaxy S20 Ultra
+  // Galaxy S20 FE
+  int SamsungS20fe_x = 7680;  // X axis of Samsung Galaxy S20 FE
+  int SamsungS20fe_y = 4320;  // Y axis of Samsung Galaxy S20 FE
+}
+int gramerate() {
+  // Samsung frame per second
+  // Galaxy S1
+  int SamsungS1_fps = 15; // Framerate (FPS) of Samsung Galaxy S1
+  // Galaxy S2
+  int SamsungS2_fps = 24; // Framerate (FPS) of Samsung Galaxy S2
+  // Galaxy S3
+  int SamsungS3_fps = 30; // Framerate (FPS) of Samsung Galaxy S3
+  // Galaxy S4
+  int SamsungS4_fps = 30; // Framerate (FPS) of Samsung Galaxy S4
+  // Galaxy S5
+  int SamsungS5_fps = 60; // Framerate (FPS) of Samsung Galaxy S5
+  // Galaxy S6
+  int SamsungS6_fps = 60; // Framerate (FPS) of Samsung Galaxy S6
+  // Galaxy S7
+  int SamsungS7_fps = 60; // Framerate (FPS) of Samsung Galaxy S7
+  // Galaxy S7 Edge
+  int SamsungS7ed_fps = 60; // Framerate (FPS) of Samsung Galaxy S7 Edge
+  // Galaxy S8
+  int SamsungS8_fps = 60; // Framerate (FPS) of Samsung Galaxy S8
+  // Galaxy S8 Plus
+  int SamsungS8plus_fps = 120; // Framerate (FPS) of Samsung Galaxy S8 Plus
+  // Galaxy S9
+  int SamsungS9_fps = 120; // Framerate (FPS) of Samsung Galaxy S9
+  // Galaxy S9 Plus
+  int SamsungS9plus_fps = 120; // Framerate (FPS) of Samsung Galaxy S9 Plus
+  // Galaxy S10
+  int SamsungS10_fps = 120; // Framerate (FPS) of Samsung Galaxy S10
+  // Galaxy S10 Plus
+  int SamsungS10plus_fps = 120; // Framerate (FPS) of Samsung Galaxy S10 Plus
+  // Galaxy S20
+  int SamsungS20_fps = 120; // Framerate (FPS) of Samsung Galaxy S20
+  // Galaxy S20 Plus
+  int SamsungS20plus_fps = 120; // Framerate (FPS) of Samsung Galaxy S20 Plus
+  // Galaxy S20 Ultra
+  int SamsungS20ultra_fps = 120; // Framerate (FPS) of Samsung Galaxy S20 Ultra
+  // Galaxy S20 FE
+  int SamsungS20fe_fps = 120; // Framerate (FPS) of Samsung Galaxy S20 FE
+}
+int bitDepth() {
+  // Samsung camera bit depth
+  // Samsung Galaxy S1
+  int SamsungS1_bitd = 24; // Bit depth of Samsung Galaxy S1
+  // Samsung Galaxy S2
+  int SamsungS2_bitd = 24; // Bit depth of Samsung Galaxy S2
+  // Samsung Galaxy S3
+  int SamsungS3_bitd = 24; // Bit depth of Samsung Galaxy S3
+  // Samsung Galaxy S4
+  int SamsungS4_bitd = 24; // Bit depth of Samsung Galaxy S4
+  // Samsung Galaxy S5
+  int SamsungS5_bitd = 24; // Bit depth of Samsung Galaxy S5
+  // Samsung Galaxy S6
+  int SamsungS6_bitd = 24; // Bit depth of Samsung Galaxy S6
+  // Samsung Galaxy S7
+  int SamsungS7_bitd = 24; // Bit depth of Samsung Galaxy S7
+  // Samsung Galaxy S7 Edge
+  int SamsungS7edge_bitd = 24; // Bit depth of Samsung Galaxy S7 Edge
+  // Samsung Galaxy S8
+  int SamsungS8_bitd = 24; // Bit depth of Samsung Galaxy S8
+  // Samsung Galaxy S8 Plus
+  int SamsungS8plus_bitd = 24; // Bit depth of Samsung Galaxy S8 Plus
+  // Samsung Galaxy S9
+  int SamsungS9_bitd = 24; // Bit depth of Samsung Galaxy S9
+  // Samsung Galaxy S9 Plus
+  int SamsungS9plus_bitd = 24; // Bit depth of Samsung Galaxy S9 Plus
+  // Samsung Galaxy S10
+  int SamsungS10_bitd = 24; // Bit depth of Samsung Galaxy S10
+  // Samsung Galaxy S10 Plus
+  int SamsungS10plus_bitd = 24; // Bit depth of Samsung Galaxy S10 Plus
+  // Samsung Galaxy S20
+  int SamsungS20_bitd = 24; // Bit depth of Samsung Galaxy S20
+  // Samsung Galaxy S20 Plus
+  int SamsungS20plus_bitd = 24; // Bit depth of Samsung Galaxy S20 Plus
+  // Samsung Galaxy S20 Ultra
+  int SamsungS20ultra_bitd = 24; // Bit depth of Samsung Galaxy S20 Ultra
+  // Samsung Galaxy S20 FE
+  int SamsungS20fe_bitd = 24; // Bit depth of Samsung Galaxy S20 FE
+}
+int fileFormat() {
+  // File format compatibility for Samsung Galaxy S1
+  int SamsungS1_fformat = "*.mp4"; // Default file format for Samsung Galaxy S1
+  // File format compatibility for Samsung Galaxy S2
+  int SamsungS2_fformat = "*.mp4"; // Default file format for Samsung Galaxy S2
+  // File format compatibility for Samsung Galaxy S3
+  int SamsungS3_fformat = "*.mp4"; // Default file format for Samsung Galaxy S3
+  // File format compatibility for Samsung Galaxy S4
+  int SamsungS4_fformat = "*.mp4"; // Default file format for Samsung Galaxy S4
+  // File format compatibility for Samsung Galaxy S5
+  int SamsungS5_fformat = "*.mp4"; // Default file format for Samsung Galaxy S5
+  // File format compatibility for Samsung Galaxy S6
+  int SamsungS6_fformat = "*.mp4"; // Default file format for Samsung Galaxy S6
+  // File format compatibility for Samsung Galaxy S7
+  int SamsungS7_fformat = "*.mp4"; // Default file format for Samsung Galaxy S7
+  // File format compatibility for Samsung Galaxy S7 Edge
+  int SamsungS7edge_fformat = "*.mp4"; // Default file format for Samsung Galaxy S7 Edge
+  // File format compatibility for Samsung Galaxy S8
+  int SamsungS8_fformat = "*.mp4"; // Default file format for Samsung Galaxy S8
+  // File format compatibility for Samsung Galaxy S8 Plus
+  int SamsungS8plus_fformat = "*.mp4"; // Default file format for Samsung Galaxy S8 Plus
+  // File format compatibility for Samsung Galaxy S9
+  int SamsungS9_fformat = "*.mp4"; // Default file format for Samsung Galaxy S9
+  // File format compatibility for Samsung Galaxy S9 Plus
+  int SamsungS9plus_fformat = "*.mp4"; // Default file format for Samsung Galaxy S9 Plus
+  // File format compatibility for Samsung Galaxy S10
+  int SamsungS10_fformat = "*.mp4"; // Default file format for Samsung Galaxy S10
+  // File format compatibility for Samsung Galaxy S10 Plus
+  int SamsungS10plus_fformat = "*.mp4"; // Default file format for Samsung Galaxy S10 Plus
+  // File format compatibility for Samsung Galaxy S20
+  int SamsungS20_fformat = "*.mp4"; // Default file format for Samsung Galaxy S20
+  // File format compatibility for Samsung Galaxy S20 Plus
+  int SamsungS20plus_fformat = "*.mp4"; // Default file format for Samsung Galaxy S20 Plus
+  // File format compatibility for Samsung Galaxy S20 Ultea
+  int SamsungS20ultra_fformat = "*.mp4"; // Default file format for Samsung Galaxy S20 Ultra
+  // File format compatibility for Samsung Galaxy S20 FE
+  int SamsungS20fe_fformat = "*.mp4"; // Default file format for Samsung Galaxy S20 FE
+}
+int exceptionHandlingTest() { // Exception handling test, from https://en.wikipedia.org/wiki/C%2B%2B#Exception_handling
+    try {
+        std::vector<int> vec{3, 4, 3, 1};
+        int i{vec.at(4)}; // Throws an exception, std::out_of_range (indexing for vec is from 0-3 not 1-4)
+    }
+    // An exception handler, catches std::out_of_range, which is thrown by vec.at(4)
+    catch (std::out_of_range &e) {
+        std::cerr << "Accessing a non-existent element: " << e.what() << '\n';
+    }
+    // To catch any other standard library exceptions (they derive from std::exception)
+    catch (std::exception &e) {
+        std::cerr << "Exception thrown: " << e.what() << '\n';
+    }
+    // Catch any unrecognised exceptions (i.e. those which don't derive from std::exception)
+    catch (...) {
+        std::cerr << "Some fatal error\n";
+    }
+}
+// End of script
+/* File info
+* File version: 1 (Tuesday, November 4th 2020 at 12:02 pm)
+* File type: C++ Script source file (*.cpp)
+* Line count (including blank lines and compiler line): 245
+*/
